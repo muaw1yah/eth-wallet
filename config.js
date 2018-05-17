64bd529fb6c387db85dfd114633c7adcb26727e8
namadi <nmuawiyah@gmail.com>
2018-05-17 08:36:46 +0100

    initial commit

---
+++ config.js
@@ -0,0 +1,5 @@
+module.exports = {
+    'secret': "mydirtylittlesecret",
+    'database': "mongodb://localhost/eth-wallet",
+    'PORT': process.env.PORT || 8080,
+}
--- db.js
+++ db.js
@@ -1,2 +1,4 @@
 var mongoose = require('mongoose');
-mongoose.connect('mongodb://yourMongoDBURIGoesHere');
+var config = require('./config');
+
+mongoose.connect(config.database);
